1f678574bd4ebc60fd1b97323908b6c4a3efaf89
Ducky229 <229193-3>
2022-11-17 16:37:09 +0300

    This is first downloading into this repository

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Лабораторная_работа___13
+{
+    class Program
+    {
+        private static double Sum(ref double x)
+        {
+            for (double i = 1; i < 10; i++)
+                x += i / Math.Pow(x, i);
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            x = 0; //Missing Sum()
+            Console.Write("X = ");
+            Console.Write(x);
+            Console.Read();
+        }
+    }
+}
